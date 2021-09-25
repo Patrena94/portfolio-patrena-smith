87b776b2a72da9d3a98967bf28acf2146a87f3d4
Vashti <patrena@sbcglobal.net>
2021-09-24 19:46:46 -0700

    updated nav bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import About from './components/About';
+import Nav from './components/Nav';
 // import logo from './logo.svg';
 import './App.css';
 
@@ -7,9 +8,9 @@ function App() {
   return (
     <div> 
       <main>
+      <Nav></Nav>
       <About></About>
       </main>
-
     </div>
   );
 }
---
+++ src/components/Nav/index.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+function Nav() {
+    const categories = [
+        {
+          name: "Portfolio",
+          description:
+            "body of work",
+        },
+        { name: "Resume", description: "Professional work Experience" },
+
+      ];
+    function categorySelected(name){
+        console.log(`${name}clicked`)
+    } 
+  return (
+    <header>
+     <h2>
+    <a href="/">
+      <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+    </a>
+  </h2>
+  <nav>
+    <ul className="flex-row">
+      <li className="mx-2">
+        <a href="#about">
+          About me
+        </a>
+      </li>
+      <li>
+        <span>Contact</span>
+      </li>
+       {categories.map((category) => (
+        <li
+          className="mx-1"
+          key={category.name}
+        >
+          <span onClick={categorySelected} >
+            {category.name}
+          </span>
+        </li>
+      ))}
+    </ul>
+  </nav>
+    </header>
+  );
+}
+
+export default Nav;
