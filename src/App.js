f87bf4fb388cf6c4c71ea29d4342d3707a2b692c
Vashti <patrena@sbcglobal.net>
2021-09-26 15:32:17 -0700

    added contact component, contact form, updated App.js to import Contact form

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import About from './components/About';
 import Nav from './components/Nav';
 import Portfolio from './components/Portfolio';
+import ContactForm from './components/Contact';
 import './App.css';
+// import ContactForm from './components/Contact';
 
 function App() {
   return (
     <div>
       <Nav></Nav> 
       <main>
+      <ContactForm></ContactForm> 
       <Portfolio></Portfolio>
       <About></About>
       </main>
